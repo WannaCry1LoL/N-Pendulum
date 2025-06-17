312c44cff9bb8c21961e8e949449daf84bba1dec
WannaCry1LoL <alex.daniellw@gmail.com>
2025-06-17 06:58:59 +0200

    Fixed form

--- Pendulum/Form1.cs
+++ Pendulum/Form1.cs
@@ -1,9 +1,39 @@
-namespace Pendulum;
+using System.Diagnostics;
 
+namespace Pendulum;
+using Pendulum.Math;
 public partial class Form1 : Form
 {
-    public Form1()
-    {
-        InitializeComponent();
-    }
+	private Panel MainArea = new DoubleBufferedPanel();
+	private NPendulum pendulum = new NPendulum();
+	private DateTime lastUpdate;
+	public Form1()
+	{
+		var timer = new System.Windows.Forms.Timer()
+		{
+			Interval = 1
+		};
+		timer.Tick += (sender, args) =>
+		{
+			var dt = DateTime.Now - lastUpdate;
+			pendulum.Update(dt.TotalMilliseconds / 2000.0 ); // this immediately explodes everything no matter what numbers i try 
+			lastUpdate = DateTime.Now;
+			MainArea.Invalidate();
+		};
+		pendulum
+			.AddN(2, 0.45 * double.Pi, 0.0);
+		InitializeComponent();
+		ResizeRedraw = true;
+		Resize += (sender, args) =>
+		{
+			MainArea.Size = ClientSize;
+		};
+		MainArea.Paint += (sender, args) =>
+		{
+			pendulum.Draw(args.Graphics, new PointF(MainArea.ClientSize.Width / 2, MainArea.ClientSize.Height / 2));
+		};
+		Controls.Add(MainArea);
+		lastUpdate = DateTime.Now;
+		timer.Start();
+	}
 }
