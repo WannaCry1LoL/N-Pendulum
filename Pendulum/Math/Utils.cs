ed6df8937ee3cc98c5246bc9d150645748e4d77d
WannaCry1LoL <alex.daniellw@gmail.com>
2025-06-26 04:30:11 +0200

    Added Utils

---
+++ Pendulum/Math/Utils.cs
@@ -0,0 +1,14 @@
+﻿namespace Pendulum.Math;
+
+public static class Utils
+{
+	public static T[][] CreateFixedSizeBuffers<T>(int n, int size)
+	{
+		var res = new T[n][];
+		for (int i = 0; i < n; i++)
+		{
+			res[i] = new T[size];
+		}
+		return res;
+	}
+}
